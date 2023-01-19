d750ec087d37048900bbf013cb20da63f4cae865
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-01-19 01:28:29 +0100

    id-producte ok

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -1,47 +1,56 @@
 <template>
   <v-container>
     <h2>Lista de productes</h2>
+    <br />
     <v-row>
-      <v-card cols="3" v-for="(p,i) in productsVue" :key="p.id">
-          <v-img max-width="200px" :src="p.thumbnail"></v-img>
-          <v-card-title>{{p.title}}</v-card-title>
-          <v-card-text>{{p.description}}</v-card-text>
-      </v-card>
+      <v-col flex v-for="p in productsVue" :key="p.id">
+        <v-card class="mx-auto" max-width="300">
+          <v-img
+            height="150px"
+            :src="p.thumbnail"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          >
+        </v-img>
+        <v-card-title v-text="p.title" style="color:yellowgreen"></v-card-title>
+          <v-card-text
+            class="text--primary"
+            v-text="p.description"
+          ></v-card-text>
+          <v-card-actions>
+            <v-btn color="blue" :to="'/producte/' + p.id"> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  data(){
-      return {
-        productsVue:null,
-      }
-    },
-  mounted(){
-        this.descarregarProductes()
-      },
-  methods:{
-    descarregarProductes(){
-      console.log('Provant api..')
-      let self = this
-      this.$axios.get('https://dummyjson.com/products')
+  data() {
+    return {
+      productsVue: [],
+    };
+  },
+  mounted() {
+    this.descarregarProductes();
+  },
+  methods: {
+    descarregarProductes() {
+      console.log("Provant api..");
+      let self = this;
+      this.$axios
+        .get("https://dummyjson.com/products")
         // Quan acabi
-        .then(
-          resposta=>{
-            console.log("M'he descarregat les dades bé",resposta.data)
-            self.productsVue = resposta.data.products
-            
-          }
-        )
+        .then((resposta) => {
+          console.log("M'he descarregat les dades bé", resposta.data);
+          self.productsVue = resposta.data.products;
+        })
         // Si hi ha errors
-        .catch(
-          error=>{
-            console.log("M'he descarregat les dades malament",error)
-          }
-        )
+        .catch((error) => {
+          console.log("M'he descarregat les dades malament", error);
+        });
     },
-    
-  }
-}
+  },
+};
 </script>
---
+++ codi/pages/producte/_idProducte.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card  class="product-card mx-auto" 
+        @mouseover="showDetails = true" 
+        @mouseleave="showDetails = false"
+        max-width="500px">
+          <v-img height="300px" :src="producteVue.thumbnail"></v-img>
+          <div class="product-info" v-if="showDetails">
+
+            <v-card-title
+              v-text="producteVue.title"
+              style="color: #2f7693"
+            ></v-card-title>
+            <v-card-title
+              v-text="producteVue.price + ' $'"
+              style="color:#708083"
+            ></v-card-title>
+            <v-rating
+              :value="producteVue.rating"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+            <div class="grey--text ms-4"></div>
+            <v-card-text
+              class="text--primary"
+              v-text="producteVue.description"
+            ></v-card-text>
+            <v-card-text 
+              class="text--primary"
+              v-text="producteVue.category"
+            ></v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" :to="'/'"> Home </v-btn>
+            </v-card-actions>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.descarregarProducte();
+  },
+  methods: {
+    descarregarProducte() {
+      let id = this.$route.params.idProducte;
+      let self = this;
+      console.log(id);
+      this.$axios
+        .get(`https://dummyjson.com/products/${id}`)
+        .then((response) => {
+          self.producteVue = response.data;
+          console.log(self.producteVue);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  data() {
+    return {
+      producteVue: {},
+      showDetails: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.product-card {
+  position: relative;
+  transition: all 0.2s ease-in-out;
+}
+
+.product-card:hover .product-info {
+  opacity: 1;
+  visibility: visible;
+}
+
+.product-info {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.9);
+  padding: 1rem;
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.2s ease-in-out;
+}
+</style>
